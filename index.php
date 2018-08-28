00a73f102c8a04599f45f30eb65b8a04a9c5e707
tender <tender@psychologies.dip.jp>
2018-08-28 20:49:41 +0900

    add new file

--- index.php
+++ index.php
@@ -2,31 +2,33 @@
 ini_set('display_errors', on );
 error_reporting(E_ALL);
 
-$no = '';
-if(isset($_POST['submit'])){
-  echo $_POST['type'];
-  echo $_POST['detail'];
-  echo $_POST['spend'];
-  echo $_POST['income'];
+if(isset($_POST['insert'])){
+    echo $_POST['item'],$_POST['detail'],$_POST['outcome'],$_POST['income'];
+    echo '<br />';
     $USER = 'finance';
     $PW = 'Finance10+*';
     $dnsinfo = "mysql:dbname=finance;host=localhost;charset=utf8";
+
+    //DBにアクセス
+    $id=0;
     try{
       $pdo = new PDO($dnsinfo,$USER,$PW);
-      $sql = "INSERT INTO kakeibo VALUES(?,?,?,?,?)";
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //デバッグ用
+      //$sql = "INSERT INTO money_records VALUES(?,?,?,?,?)";
+      $sql = "INSERT INTO money_records (id,item,detail,outcome,income) VALUES(?,?,?,?,?)";
       $stmt = $pdo->prepare($sql);
-      $array = array($no,$_POST['type'],$_POST['detail'],$_POST['spend'],$_POST['income']);
-      var_dump ($array);
+      $array = array($id,$_POST['item'],$_POST['detail'],$_POST['outcome'],$_POST['income']);
       $res = $stmt->execute($array);
-      $ok ='登録が完了しました。<br />';
-      #$ok .= $date;
+      //$res = $stmt->execute(null);
+      echo date("Y.m.d|H:i:s");
       }catch(Exception $e){
         $res = $e->getMessage();
+        echo $res;
         echo '失敗！';
       }
 }
 $ItemName = array("食費","書籍費","学費","美容費","医療費","被服費","年金","保険","家電","PC関連費","文具費","日用品","雑費");
-$items = "<select name='type'>";
+$items = "<select name='item'>";
 $items .= "<option value=''>" ."選択してください";
 for ($i=0;$i<13;$i++){
   $items .= "<option value='$ItemName[$i]'>$ItemName[$i]</option>";
@@ -34,9 +36,10 @@ for ($i=0;$i<13;$i++){
 $items .= "</select>";
 
 if(isset($_POST['detail'])){
-  $type = $_POST['type'];
-  $itemname = $_POST['detail'];
-  $result =  "「{$type}」として「{$itemname}」を入力しました。";
+  $item = $_POST['item'];
+  $outcome = $_POST['detail'];
+  $result =  "「{$item}」として「{$outcome}」を入力しました。";
+  //echo $result;
 }
 ?>
 
@@ -46,6 +49,7 @@ if(isset($_POST['detail'])){
   <meta charset="utf-8">
   <title>家計簿(単式簿記)システム</title>
   <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" type="text/css" href="../css/style2.css" />
   <link rel="stylesheet" href="./css/reset.css">
 </head>
 <body>
@@ -58,13 +62,13 @@ if(isset($_POST['detail'])){
   </div>
   <nav>
     <ul>
-      <li><a href="/>">入力</a></li>
+      <li><a href="/">入力</a></li>
       <li><a href="list.php">リスト表記</a></li>
       <li><a href="graphical.php">グラフ</a></li>
     </ul>
   </nav>
   <main>
-    <form action="" method="post">
+    <form action="" method="POST">
       <table border="1">
         <tr>
           <th align="center">
@@ -87,15 +91,14 @@ if(isset($_POST['detail'])){
             <input type="text" name="detail" size="10" value=""/>
           </td>
           <td>
-            <input type="text" name="spend" value="0" size="5">
+            <input type="text" name="outcome" value="0" size="5">
           </td>
           <td>
             <input type="text" name="income" value="0" size="5">
           </td>
       </table>
-      <input type="submit" name="submit" value="確定">
+      <input type="submit" name="insert" value="送信">
     </form>
-    <?php// echo $result;?>
   </main>
 <footer>
   <address>
